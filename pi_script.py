345b2bf85dddce1a5e16783e6e316b65b9f02ffa
John Matsoukas <62017772+John-Progr@users.noreply.github.com>
2025-10-13 21:29:08 +0300

    Update pi_script.py
    removed some messages due to debugging reasons

--- pi_script.py
+++ pi_script.py
@@ -319,7 +319,7 @@ class MqttDevice:
         self.client.loop_start()
 
     def run(self):
-        self.logger.info("📡 Listening for messages...")
+        
 
 def main():
     device = MqttDevice()
